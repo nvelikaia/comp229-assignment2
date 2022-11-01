6a69ac4697fea617efcd3c6ea5f2818bbc74fc9c
Nadia Velikaia <nvelikai@my.centennialcollege.ca>
2022-11-01 14:54:33 -0700

    updated db.js file with monfodb server

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 module.exports = 
 {
-    "URI": "mongodb://127.0.0.1/contact_list",
-    //"URI": "mongodb+srv://thomas:Hq3DKYN2NaAqGyyV@mongodbserver.k15hk.azure.mongodb.net/book_store?retryWrites=true&w=majority",
+    //"URI": "mongodb://127.0.0.1/contact_list",
+    "URI": "mongodb+srv://nvelikaia:ETbrSA9gbFRf8iu0@mongodbserver.xsgmbeq.mongodb.net/?retryWrites=true&w=majority",
     "Secret": 'SomeSecret'
 }
