3316a16fd4abab455fe3dd03bcfaf10ac43d78dc
Nadia Velikaia <nvelikai@my.centennialcollege.ca>
2022-11-01 14:05:01 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# comp229-assignment2
+# comp229-assignment2
---
+++ server/config/db.js
@@ -0,0 +1,6 @@
+module.exports = 
+{
+    "URI": "mongodb://127.0.0.1/contact_list",
+    //"URI": "mongodb+srv://thomas:Hq3DKYN2NaAqGyyV@mongodbserver.k15hk.azure.mongodb.net/book_store?retryWrites=true&w=majority",
+    "Secret": 'SomeSecret'
+}
